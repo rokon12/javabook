eceb60d8b3b7329094e7707aae274745ba592f76
Bazlur Rahman Rokon <anm_brr@yahoo.com>
2016-02-29 05:13:40 +0600

    Add chapter 02

---
+++ chapter02-syntax/src/com/bazlur/java/syntax/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.bazlur.java.syntax;
+
+import java.util.HashMap;
+
+public class HelloWorld {
+	protected final String hello = "value";
+
+	public static void main(String[] args) {
+	}
+}
---
+++ chapter02-syntax/src/com/bazlur/java/syntax/HelloWorld2.java
@@ -0,0 +1,13 @@
+package com.bazlur.java.syntax;
+
+public class HelloWorld2 {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+		System.out.println();
+		System.out.print("Hello, world!");
+		System.out.println("Hello,");
+		System.out.print(" ");
+		System.out.print("world!");
+		System.out.println("Hello, world!");
+	}
+}
