70f07175a80547f557cb1b581140f44458afdb86
Bazlur Rahman Rokon <anm_brr@yahoo.com>
2016-02-29 06:57:33 +0600

    Rename project name

---
+++ chapter02/src/com/bazlur/java/syntax/HelloWorld3.java
@@ -0,0 +1,20 @@
+package com.bazlur.java.syntax;
+
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * @author Bazlur Rahman Rokon
+ * @since 9/20/14.
+ */
+public class HelloWorld3 {
+    public static void main(String[] args) {
+        // Prints Hello, World! on standard output.
+        System.out.println("Hello, world!");
+
+        /*
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }*/
+    }
+}
